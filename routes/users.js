38e8e74d27358953cf7a2190227af4f0c9856d97
DanielaMGX <39426781+DanielaMGX@users.noreply.github.com>
2020-07-26 21:14:56 -0500

    LAB 5

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,38 @@
+  
 const express = require('express');
 const router = express.Router();
+
+
+/*Trabajo realizado por: 
+Laura Daniela Monsalve
+Andrea Calderón Zuluaga */
+
 
 /* GET users listing. */
 function usersRouter(connection, db) {
   router.get('/', async function (req, res, next) {
     // const connection = await connect();
-    const query = req.query.id ? `select first_name, last_name from users where id = ${req.query.id} and role = 'Vendedor'`
-      : `select first_name, last_name from users where role = 'Vendedor'`;
+    const query = req.query.id ? `select first_name, last_name from users where role = 'Vendedor' and id = :id`
+      : `select first_name, last_name from users where role ='Vendedor'`;
 
     // query to database. Mysql and Oracle modules have different ways to query, this is why the if is needed.
     if (db === 'oracle') {
-      try {
-        const results = await connection.execute(query, []);
-        res.send(results.rows);
-      } catch (err) {
-        console.log('Ouch!', err)
+        try {
+          const results = req.query.id ? await connection.execute(query, [req.query.id]): await connection.execute(query, []);
+          res.send(results.rows);
+        } catch (err) {
+          console.log('Ouch!', err);
+        }
+      } else {
+        const request = connection.query(query, (error, results, fields) => {
+          if (error) throw error;
+          res.send(results);
+        })
+        console.log(request.sql)
       }
-    } else {
-      const request = connection.query(query, (error, results, fields) => {
-        if (error) throw error;
-        res.send(results);
-      })
-      console.log(request.sql)
-    }
   });
   return router;
 }
 module.exports = usersRouter;
+
+/* Solución punto 3: http://localhost:3000/users?id='' or 1=1-- */
